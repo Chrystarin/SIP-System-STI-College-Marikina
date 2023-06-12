b7d9d55d8a16579dbffec4d26c29f858944eac12
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-06-13 03:18:32 +0800

    UI

---
+++ client/src/Pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './LandingPage.scss';
+import Background from '../../Images/Resources/WebsiteResources/LandingPageBackground.png'
+import Navigation from '../../Layouts/Navigation'; 
+const LandingPage: React.FC = () => {
+    return <>
+        <Navigation/>
+        <div className='Container'>
+            <header>
+                <div className='MainSection'>
+                    <div>
+                    </div>
+                    
+                </div>
+            </header>
+        </div>
+        
+        
+    </>
+}
+
+export default LandingPage
